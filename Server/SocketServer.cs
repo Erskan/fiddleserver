8ac3d6285c9be1185e7ae266ebfc39f8a35f30e3
Erik Mattsson Mårn <erikmattssonmarn@gmail.com>
2016-08-14 21:21:57 +0200

    Created sendmessage functions

--- Server/SocketServer.cs
+++ Server/SocketServer.cs
@@ -13,6 +13,9 @@ namespace FiddleServer.SocketServer
 {
     class SocketServer
     {
+        //  TODO: Handle multiple connections at once
+        private WebSocket socket;
+
         /// <summary>
         /// Starts the Websocket server by listening for requests
         /// </summary>
@@ -43,7 +46,7 @@ namespace FiddleServer.SocketServer
         /// Handle incoming requests
         /// </summary>
         /// <param name="request">WebSocket request made to the server</param>
-        public async void ProcessRequest(HttpListenerContext request)
+        private async void ProcessRequest(HttpListenerContext request)
         {
             WebSocketContext socketContext = null;
             string clientIP = request.Request.RemoteEndPoint.Address.ToString();
@@ -60,7 +63,7 @@ namespace FiddleServer.SocketServer
                 return;
             }
 
-            WebSocket socket = socketContext.WebSocket;
+            socket = socketContext.WebSocket;
             try
             {
                 byte[] receiveBuffer = new byte[1024];
@@ -74,8 +77,6 @@ namespace FiddleServer.SocketServer
                     }
                     else
                     {
-                        //Console.WriteLine("Message recieved.");
-                        //await socket.SendAsync(new ArraySegment<byte>(receiveBuffer, 0, socketResult.Count), WebSocketMessageType.Binary, socketResult.EndOfMessage, CancellationToken.None);
                         HandleMessage(socketResult, receiveBuffer);
                     }
                 }
@@ -91,11 +92,37 @@ namespace FiddleServer.SocketServer
             }
         }
 
+        /// <summary>
+        /// Handles incoming messages
+        /// </summary>
+        /// <param name="message">WebSocketReceiveResult to determine things about the message</param>
+        /// <param name="messageData">The message data in byte format</param>
         private void HandleMessage(WebSocketReceiveResult message, byte[] messageData)
         {
             string decodedMessage = Encoding.UTF8.GetString(messageData).Substring(0, message.Count);
             Console.WriteLine(decodedMessage);
             var jsonMessage = JsonConvert.DeserializeObject(decodedMessage);
+            // TODO: Game logic
+            SendMessage("Message handled.");
+        }
+
+        /// <summary>
+        /// Sends a text message to the client
+        /// </summary>
+        /// <param name="message">The message in string format</param>
+        private void SendMessage(string message)
+        {
+            SendMessage(Encoding.UTF8.GetByteCount(message), Encoding.UTF8.GetBytes(message));
+        }
+
+        /// <summary>
+        /// Sends a binary message to the client
+        /// </summary>
+        /// <param name="messageLength">Number of bytes to send</param>
+        /// <param name="messageData">The byte array containing the message</param>
+        private async void SendMessage(int messageLength, byte[] messageData)
+        {
+            await socket.SendAsync(new ArraySegment<byte>(messageData, 0, messageLength), WebSocketMessageType.Binary, true, CancellationToken.None);
         }
     }
 }
