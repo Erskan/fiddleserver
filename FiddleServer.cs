97ddb168fd0363c32bacca78ed7e31d76ffbcf3d
Erik Mattsson Mårn <erikmattssonmarn@gmail.com>
2016-08-14 18:06:58 +0200

    Basic server listening on port 9000 for WebSocket connections

---
+++ FiddleServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FiddleServer.SocketServer;
+
+namespace FiddleServer
+{
+    class FiddleServer
+    {
+        static void Main(string[] args)
+        {
+            SocketServer.SocketServer ws = new SocketServer.SocketServer();
+            ws.Start("http://localhost:9000/websocket/");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/SocketServer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace FiddleServer.SocketServer
+{
+    class SocketServer
+    {
+        /// <summary>
+        /// Starts the Websocket server by listening for requests
+        /// </summary>
+        public async void Start(string listenerPrefix)
+        {
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add(listenerPrefix);
+            httpListener.Start();
+            Console.WriteLine("Listening for incoming HTTP calls...");
+
+            while (true)
+            {
+                HttpListenerContext httpContext = await httpListener.GetContextAsync();
+                if(httpContext.Request.IsWebSocketRequest)
+                {
+                    ProcessRequest(httpContext);
+                }
+                else
+                {
+                    httpContext.Response.StatusCode = 400;
+                    httpContext.Response.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handle incoming requests
+        /// </summary>
+        /// <param name="request">WebSocket request made to the server</param>
+        public async void ProcessRequest(HttpListenerContext request)
+        {
+            WebSocketContext socketContext = null;
+            string clientIP = request.Request.RemoteEndPoint.Address.ToString();
+            try
+            {
+                socketContext = await request.AcceptWebSocketAsync(subProtocol: null);
+                Console.WriteLine("{0} connected to the server.", clientIP);
+            }
+            catch (Exception e)
+            {
+                request.Response.StatusCode = 500;
+                request.Response.Close();
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            WebSocket socket = socketContext.WebSocket;
+            try
+            {
+                byte[] receiveBuffer = new byte[1024];
+                while(socket.State == WebSocketState.Open)
+                {
+                    WebSocketReceiveResult socketResult = await socket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                    if(socketResult.MessageType == WebSocketMessageType.Close)
+                    {
+                        await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                        Console.WriteLine("{0} disconnected from the server.", clientIP);
+                    }
+                    else
+                    {
+                        await socket.SendAsync(new ArraySegment<byte>(receiveBuffer, 0, socketResult.Count), WebSocketMessageType.Binary, socketResult.EndOfMessage, CancellationToken.None);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if (socket != null)
+                    socket.Dispose();
+            }
+        }
+    }
+}
